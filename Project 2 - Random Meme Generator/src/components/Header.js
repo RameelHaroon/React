890b89bdd8ec22f43f0c7cfd22647a95227b1976
Rameel Haroon <101657180+RameelHaroon@users.noreply.github.com>
2023-11-02 20:32:01 +0500

    Add files via upload

---
+++ Project 2 - Random Meme Generator/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+export default function Header(){
+
+    return(
+        <div className="Header">
+            <div className="Header-Section">
+                <img className="Header--Icon" src="../images/Troll Face.png"></img>
+                <h2  className="Header--heading">Meme Generator</h2>
+            </div>
+                <p  className="Header--passage">React Course - Project 3</p>
+        </div>
+    )
+}
